4ddbdbc4661e372ec6a7be54e604c4dd9eaa8077
Chad Lou <chadlou@us.ibm.com>
2017-07-31 12:12:34 -0500

    pops the same one as pushed

--- server/03-stack/stack.spec.js
+++ server/03-stack/stack.spec.js
@@ -2,14 +2,18 @@ const LIMIT = 3;
 
 let makeStack = () => {
   let size = 0;
+  const array = [];
   return {
     isEmpty: () => size === 0,
-    push: () => {
+    push: (number) => {
       size++;
+      array.push(number);
       if (size > LIMIT) throw new Error('Cannot exceed ', LIMIT);
     },
     pop: () => {
+      if (size <= 0) throw new Error('Cannot pop an empty stack');
       size--;
+      return array.pop();
     },
     size: () => size
   };
@@ -55,14 +59,29 @@ describe.only('the stack spec', () => {
    stack.push();
    stack.push();
    stack.push();
-   (() => {
+   const overFlowFunction = () => {
      stack.push();
-   }).should.throw('Cannot exceed ', LIMIT);
+   };
+   // (() => {
+   //      stack.push();
+   //    }).should.throw('Cannot exceed ', LIMIT);
+   //stack.push.should.throw('Cannot exceed ', LIMIT);
+   overFlowFunction.should.throw('Cannot exceed ', LIMIT);
  });
 
- it('under-flows');
+ it('under-flows', () => {
+   (stack.pop).should.throw('Cannot pop an empty stack');
+   stack.push();
+   stack.pop();
+   (stack.pop).should.throw('Cannot pop an empty stack');
+ });
 
- it('pops the same one pushed');
+ it('pops the same one pushed', () => {
+   stack.push(3);
+   stack.pop().should.equal(3);
+   stack.push('red');
+   stack.pop().should.equal('red');
+ });
 
  it('pops the same two pushed');
 
