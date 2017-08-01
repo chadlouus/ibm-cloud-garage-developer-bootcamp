ba678dc0b5bd5da580c1851998c9f281cf818325
Chad Lou <chadlou@us.ibm.com>
2017-08-01 10:56:11 -0500

    formatted by Cmd_Option_L

--- server/03-stack/stack.js
+++ server/03-stack/stack.js
@@ -1,4 +1,8 @@
-import {STACK_OVERFLOW_ERROR, STACK_UNDERFLOW_ERROR, NEGATIVE_CAPACITY} from './stack-errors';
+import {
+  STACK_OVERFLOW_ERROR,
+  STACK_UNDERFLOW_ERROR,
+  NEGATIVE_CAPACITY
+} from './stack-errors';
 
 export class Stack {
   constructor() {
@@ -6,34 +10,34 @@ export class Stack {
     this.capacity = 3;
   }
 
-  isEmpty () {
+  isEmpty() {
     return this.stackArray.length === 0;
   }
 
-    size() {
-      return this.stackArray.length;
-    }
+  size() {
+    return this.stackArray.length;
+  }
 
-    getCapacity() {
-      return this.capacity;
-    }
+  getCapacity() {
+    return this.capacity;
+  }
 
-    push(value) {
-      if (this.stackArray.length >= this.capacity) {
-        throw new Error(STACK_OVERFLOW_ERROR);
-      }
-      this.stackArray.push(value);
+  push(value) {
+    if (this.stackArray.length >= this.capacity) {
+      throw new Error(STACK_OVERFLOW_ERROR);
     }
+    this.stackArray.push(value);
+  }
 
-    pop() {
-      if (this.stackArray.length <= 0) {
-        throw new Error(STACK_UNDERFLOW_ERROR);
-      }
-      return this.stackArray.pop();
+  pop() {
+    if (this.stackArray.length <= 0) {
+      throw new Error(STACK_UNDERFLOW_ERROR);
     }
+    return this.stackArray.pop();
+  }
 
-    setCapacity(value) {
-      if (value <= 0) throw new Error(NEGATIVE_CAPACITY);
-      this.capacity = value;
-    }
+  setCapacity(value) {
+    if (value <= 0) throw new Error(NEGATIVE_CAPACITY);
+    this.capacity = value;
+  }
 }
