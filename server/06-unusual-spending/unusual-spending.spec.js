7f6abc31815e2bf397acb4dd6059fda60f58939a
Chad Lou <chadlou@us.ibm.com>
2017-08-03 13:46:42 -0500

    initial implementation

--- server/06-unusual-spending/unusual-spending.spec.js
+++ server/06-unusual-spending/unusual-spending.spec.js
@@ -1,5 +1,18 @@
+import {replace, when, verify} from '../../test-helper';
+
 describe.only('unusual spending', () => {
   it('canary test', () => {
     true.should.be.true();
   });
+  it('should orchestrate categorize, payment and email', () => {
+    const fetch = replace('./fetch').fetch;
+    const categorize = replace('./categorize').categorize;
+    const email = replace('./email').email;
+
+    let unusualSpending = replace('./unusual-spending').unusualSpending;
+    when(fetch('user-id')).thenReturn('payments');
+    when(categorize('payments')).thenReturn('categorized-payments');
+    unusualSpending('user-id');
+    verify(email("user-id", 'categorized-payments'));
+  })
 });
